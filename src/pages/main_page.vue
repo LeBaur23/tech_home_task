8e9ae870c5e0447993a7e830751e98409afce893
baron <19970620baur>
2019-07-02 15:26:41 +0600

    first commit

--- src/pages/main_page.vue
+++ src/pages/main_page.vue
@@ -8,6 +8,7 @@
       <chocomart-icon :icon="'bell'" :color="'white'" :font_size="'25px'"></chocomart-icon>
       <chocomart-icon :icon="'mark'" :color="'white'" :font_size="'25px'"></chocomart-icon>
       <chocomart-icon :icon="'home'" :color="'white'" :font_size="'25px'"></chocomart-icon>
+      <!--test-->
     </div>
   </div>
 </template>
